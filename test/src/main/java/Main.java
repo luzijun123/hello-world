5eddff245108c6c1fb6c71ddc1501f6ba409bcf7
wanglujing <wanglujing@tuniu.com>
2019-12-06 17:34:55 +0800

    master提交

---
+++ test/src/main/java/Main.java
@@ -0,0 +1,13 @@
+/**
+ * 描述:
+ * test
+ *
+ * @author lujing.wang
+ * @create 2019-12-06 4:58 PM
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
